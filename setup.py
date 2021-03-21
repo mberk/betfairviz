585bf46db949914b9f4e81007d0ee17c9ecfd382
Maurice Berk <maurice@mauriceberk.com>
2021-03-21 10:16:35 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='betfairviz',
+    version='0.0.1',
+    description='Create visualisations of Betfair order books',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Maurice Berk',
+    author_email='maurice@mauriceberk.com',
+    url='https://github.com/mberk/betfairviz',
+    packages=['betfairviz'],
+    tests_require=[],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5',
+)
