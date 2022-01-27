d2cf8455efc87659e78d57d961316f23ce08452b
Maurice Berk <maurice@mauriceberk.com>
2022-01-27 14:05:18 +0000

    Apply black formatting

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
 from setuptools import setup
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
-requires = [
-    'betfairlightweight',
-    'betfairutil[files]>=0.0.2',
-    'ipywidgets'
-]
+requires = ["betfairlightweight", "betfairutil[files]>=0.0.2", "ipywidgets"]
 
 setup(
-    name='betfairviz',
-    version='0.0.1',
-    description='Create visualisations of Betfair order books',
+    name="betfairviz",
+    version="0.0.1",
+    description="Create visualisations of Betfair order books",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Maurice Berk',
-    author_email='maurice@mauriceberk.com',
-    url='https://github.com/mberk/betfairviz',
-    packages=['betfairviz'],
+    long_description_content_type="text/markdown",
+    author="Maurice Berk",
+    author_email="maurice@mauriceberk.com",
+    url="https://github.com/mberk/betfairviz",
+    packages=["betfairviz"],
     install_requires=requires,
     tests_require=[],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires=">=3.5",
 )
