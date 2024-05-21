dc2c1ea1c372131c94cbcad473406905a124368b
Maurice Berk <maurice@mauriceberk.com>
2024-05-21 07:44:27 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r") as f:
     long_description = f.read()
 
-requires = ["babel", "betfairlightweight", "betfairutil[files]>=0.0.2", "ipywidgets"]
+requires = ["babel", "betfairlightweight", "betfairutil[files]>=0.0.2", "ipywidgets", "plotly"]
 
 setup(
     name="betfairviz",
